70df09e07fb1e57034eb4203b9c2cf9b0e4b31f9
geraldojrcg <geraldojrcg@gmail.com>
2018-04-24 16:33:14 -0300

    v2

--- instrucao.cpp
+++ instrucao.cpp
@@ -18,9 +18,14 @@ Instrucao::Instrucao(string inst, string opcode, string op1, string op2, string 
 }
 vector<string> split(string s){
     std::istringstream iss(s);
-    std::vector<std::string> result{
-        std::istream_iterator<std::string>(iss), {}
-    };
+    std::vector<std::string> result;
+    std::string token;
+    while(getline(iss, token, ' ')) {
+       result.push_back(token);
+       while(getline(iss, token, ',')) {
+        result.push_back(token);
+       }
+    }
     return result;
 }
 vector <Instrucao*> creatinst(int qtd){
@@ -31,7 +36,7 @@ vector <Instrucao*> creatinst(int qtd){
     string instrucao;
     getline(cin, instrucao);
     while(i < qtd){
-        cout << "instrucao "<<i<<" :\n";
+        cout << "instrucao "<<i+1<<" :\n";
         getline(cin, instrucao);
         res = split(instrucao);
         if(res.size() < 3){
@@ -51,24 +56,50 @@ void print_ciclo(vector <Instrucao*> instrucoes,int qtdciclo){
     for(int i =0; i<qtdciclo; i++){
         cout << "--------- ciclo "<<i+1<<" -----------\n";
         for(int j = 0; j<5; j++)
-            cout << etapas[j]<< " -- "<< instrucoes[1]->inst << endl;
+            cout << etapas[j]<< " -- "<< instrucoes[0]->inst << endl;
     }
 }
+/*
+add $t0,$zero,$v0
+add $t1,$t0,$t2
+sub $s0,$t3,$t1
+lw $t4,4($s0)
+beq $s0,$t4,9
+add $t1,$t2,$t3
+lw $t0,128($t1)
+lw $t5,128($t1)
+lw $t5,128($t1)
+*/
 void simulator(vector <Instrucao*> instrucoes, int qtd){
     bool dependencia = false;
-    int qtdciclo = 5+(qtd-2);
+    int qtdciclo = 5;
     cout << "instruções\n";
-    for(auto ins : instrucoes)
+    //printa todas as instrucoes
+    for(auto ins : instrucoes){
         cout << ins->inst << "\n";
-    if((instrucoes[0]->opcode == "add" or instrucoes[0]->opcode == "sub") or instrucoes[0]->opcode == "addi"){
-        if(instrucoes[0]->op2 == instrucoes[1]->op1 or instrucoes[0]->op3 == instrucoes[1]->op1){
-            dependencia = true;
-            qtdciclo +=4;
+    }
+
+    for(int i =0; i< qtd-1; i++){
+        if((instrucoes[i]->opcode == "add" or instrucoes[i]->opcode == "sub") or instrucoes[i]->opcode == "addi"){
+            if((instrucoes[i]->op1 == instrucoes[i+1]->op2) or (instrucoes[i]->op1 == instrucoes[i+1]->op3)){
+                dependencia = true;
+                qtdciclo +=4;
+            }else{
+                qtdciclo ++;
+            }
+        }else if(instrucoes[i]->opcode == "lw" or instrucoes[i]->opcode == "sw"){
+            if((instrucoes[i]->op1 == instrucoes[i+1]->op2) or (instrucoes[i]->op1 == instrucoes[i+1]->op3)){
+                dependencia = true;
+                qtdciclo +=4;
+            }else{
+                qtdciclo ++;
+            }
+        }else if(instrucoes[i]->opcode == "j"){
+            i = stoi(instrucoes[i]->op1)-1;
+            qtdciclo++;
         }else{
-            qtdciclo += 1;
+            qtdciclo +=1;
         }
-    }else{
-        qtdciclo += 1;
     }
     print_ciclo(instrucoes, qtdciclo);
 }
